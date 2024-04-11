5ee47cc43c02c2beec804dcbdecc3a4823555c21
sqz269 <yin2690855381@gmail.com>
2024-04-10 23:38:56 -0400

    Removed button to copy user's access token

--- src/components/AccountButton.vue
+++ src/components/AccountButton.vue
@@ -1,18 +1,34 @@
 <template>
   <div v-if="authService?.isInitialized.value">
-    <q-btn v-if="!authService?.isAuthenticated.value" @click="authService?.login">
+    <q-btn
+      v-if="!authService?.isAuthenticated.value"
+      @click="authService?.login"
+    >
       Login
     </q-btn>
 
-    <q-btn-dropdown v-else :icon="outlinedAccountCircle" flat rounded>
+    <q-btn-dropdown
+      v-else
+      :icon="outlinedAccountCircle"
+      flat
+      rounded
+    >
       <q-list>
-        <q-item clickable v-close-popup @click="onAccountDropdownClick">
+        <q-item
+          clickable
+          v-close-popup
+          @click="onAccountDropdownClick"
+        >
           <q-item-section>
             <q-item-label>Account</q-item-label>
           </q-item-section>
         </q-item>
 
-        <q-item clickable v-close-popup @click="onAccountDropdownClick">
+        <q-item
+          clickable
+          v-close-popup
+          @click="onAccountDropdownClick"
+        >
           <q-item-section>
             <q-item-label>Settings</q-item-label>
           </q-item-section>
@@ -20,14 +36,17 @@
 
         <q-separator />
 
-        <q-item clickable v-close-popup @click="onAccountDropdownClick">
+        <q-item
+          clickable
+          v-close-popup
+          @click="onAccountDropdownClick"
+        >
           <q-item-section>
             <q-item-label>Logout</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
     </q-btn-dropdown>
-    <q-btn @click="showAccessTokenAsBearer">Reveal Auth Token</q-btn>
   </div>
 
   <span v-else><q-spinner /> Initializing Authentication</span>
@@ -43,19 +62,5 @@ const authService = inject<AuthenticationService>('authService');
 
 const onAccountDropdownClick = (evt: Event) => {
   console.log('Item clicked', evt);
-};
-
-const setClipboard = (text: string) => {
-  navigator.clipboard.writeText(text);
-};
-
-const showAccessTokenAsBearer = () => {
-  authService?.refreshAccessToken().then(() => {
-    const bearer = `${authService?.getAccessToken()}`;
-    // alert(bearer);
-    setClipboard(bearer);
-  }, (error) => {
-    console.error('Error refreshing access token', error);
-  });
 };
 </script>
