a225a48692648f8724b72e949bc74856e369b67b
sqz269 <36668060+sqz269@users.noreply.github.com>
2024-06-19 17:16:07 -0400

    Update GlobalConfiguration.ts

--- src/GlobalConfiguration.ts
+++ src/GlobalConfiguration.ts
@@ -1,7 +1,7 @@
 import { Level } from "pino";
 
 export default class GlobalConfiguration {
-  public static WEB_HOST_PUBLIC_PATH = 'tlmc-player-vue';
+  public static WEB_HOST_PUBLIC_PATH = '';
   public static API_BASE_URL = 'https://staging-api.marisad.me';
 
   public static LOGGING_LEVEL: Level = 'debug';
