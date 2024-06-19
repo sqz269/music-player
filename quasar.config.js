1a5f9608ec92bda725e0816843b07236ae301a09
sqz269 <36668060+sqz269@users.noreply.github.com>
2024-06-19 17:02:27 -0400

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: 'tlmc-player-vue',
+      // publicPath: 'tlmc-player-vue',
       // analyze: true,
       // env: {},
       // rawDefine: {}
