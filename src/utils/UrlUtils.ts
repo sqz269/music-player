809779ab327ad9c46d371d4d6cc37e2bbb41299b
sqz269 <yin2690855381@gmail.com>
2024-06-24 17:33:40 -0400

    Added search on youtube for tracks

--- src/utils/UrlUtils.ts
+++ src/utils/UrlUtils.ts
@@ -2,4 +2,8 @@ export class UrlUtils {
   static openUrlInNewTab(url: string): void {
     window.open(url, '_blank');
   }
+
+  static constructYouTubeSearchQuery(query: string): string {
+    return `https://www.youtube.com/results?search_query=${query}`;
+  }
 }
